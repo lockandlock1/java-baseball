48e8e4388d3ae178a7e08e3b81c74177072fd6cd
redkyu <hongstar2311@gmail.com>
2020-11-17 11:00:49 +0900

    기능목록 정리

---
+++ src/main/java/Application.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Application {
+
+
+  public static void main(String[] args) {
+    int com[] = new int[3];
+    int player[] = new int[3];
+    Scanner in = new Scanner(System.in);
+
+    while(true) {
+
+    }
+  }
+}
+
+
+
